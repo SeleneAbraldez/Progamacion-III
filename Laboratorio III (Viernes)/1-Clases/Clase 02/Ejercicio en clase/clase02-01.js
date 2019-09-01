4391ff344ea4284951276fd75ae168a30a78165a
Selene <Selene1999@gmail.com>
2019-08-31 21:27:38 -0300

    separado

--- Laboratorio III (Viernes)/1-Clases/Clase 02/Ejercicio en clase/clase02-01.js
+++
@@ -1,6 +0,0 @@
-function Saludo() {
-    var nombre = document.getElementById("nombretxt").value;
-    var edad = document.getElementById("edadtxt").value;
-    alert(nombre + " " + edad);
-    console.log(nombre + " " + edad);
-}
