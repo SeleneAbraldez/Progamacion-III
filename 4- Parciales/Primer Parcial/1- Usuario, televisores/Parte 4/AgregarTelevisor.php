0ae6ce55da253d3833dae0afed0fe38786890522
Selene <seleabraldez@gmail.com>
2019-10-05 20:20:57 -0300

    Pp parte4 COMPLETO PULIDO

--- 4- Parciales/Primer Parcial/1- Usuario, televisores/Parte 4/AgregarTelevisor.php
+++ 4- Parciales/Primer Parcial/1- Usuario, televisores/Parte 4/AgregarTelevisor.php
@@ -10,10 +10,9 @@ $foto = isset($_FILES["foto"]["name"]) ? $_FILES["foto"]["name"] : NULL;
 
 $extension = pathinfo($foto, PATHINFO_EXTENSION);
 //con el nombre formado por el tipo punto paisOrigen punto hora, minutos y segundos del alta (Ejemplo: led.china.105905.jpg).
-$nombreFoto= $televisor->tipo . "." . $televisor->paisOrigen. "." . date("Gis") . "." . $extension;
+$nombreFoto= $tipo . "." . $pais. "." . date("Gis") . "." . $extension;
 // La imagen guardarla en “./televisores/imagenes/”, 
 $destino = "./televisores/imagenes/" . $nombreFoto;
-
 $televisor = new Televisor($tipo, $precio, $pais, $nombreFoto);
 $retornoJson= new stdClass();
 
