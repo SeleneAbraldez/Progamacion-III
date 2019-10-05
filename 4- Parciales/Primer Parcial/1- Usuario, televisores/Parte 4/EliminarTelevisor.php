c376c5333bb5cc4d6db2d52f288f7a94f28c6051
Selene <seleabraldez@gmail.com>
2019-10-05 20:11:11 -0300

    PP parte 4 COMPLETO

---
+++ 4- Parciales/Primer Parcial/1- Usuario, televisores/Parte 4/EliminarTelevisor.php
@@ -0,0 +1,37 @@
+<?php
+require_once "./clases/Televisor.php";
+// EliminarTelevisor.php: Si recibe un tipo por GET, retorna si el televisor está en la base o no (mostrar mensaje).
+$tipoGet = isset($_GET['tipo']) ? $_GET['tipo'] : NULL;
+$teleTraer = new Televisor();
+$televisores = $teleTraer->Traer();
+$retorno = "No se encuetra en la base.";
+foreach ($televisores as $tele) {
+   if($tele->tipo == $tipoGet){
+        $retorno = "Se encuentra en la base!";
+        break;
+   }
+}
+echo $retorno;
+
+
+$retornoJson= new stdClass();
+// Si recibe el tipo por POST, con el parámetro “accion” igual a "borrar", se deberá borrar el televisor 
+$tipoPost = isset($_POST['tipo']) ? $_POST['tipo'] : NULL;
+$accionPost= isset($_POST['accion']) ? $_POST['accion'] : NULL;
+//(invocando al método Eliminar). Si se pudo borrar en la base de datos redirigir hacia Listado.php.
+if($accionPost=="borrar")
+{
+  $teleEliminar = new Televisor();
+  if($teleEliminar->Eliminar($tipoPost))
+  {
+    header("location:Listado.php");
+  }
+  else
+  {     // Si no se pudo borrar, mostrar un JSON que contendrá: éxito(bool) y mensaje(string) indicando lo acontecido.
+    $retornoJson->exito = false;
+    $retornoJson->mensaje = "No se pudo borrar el televisor";
+  }
+  echo json_encode($retornoJson);
+}
+
+?>
